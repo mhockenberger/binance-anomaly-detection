6fbb753fe4d1ac7562cb8762e89db7291e4bc7da
mhockenberger <m.hockenberger@tum.de>
2021-05-09 00:04:11 +0200

    Add Binance anomaly logic

---
+++ README.md
@@ -0,0 +1,8 @@
+# Binance Anomaly Detection
+
+Real-time anomaly detection based on the percentage price change of the cryptocurrency lowest of the low price since the previous 1h open.
+
+## Usage
+```sh
+binance-anomaly.py [-h] [--symbol [SYMBOL]] 
+```
---
+++ binance-anomaly.py
@@ -0,0 +1,115 @@
+import argparse
+
+import logging
+import time
+import os
+import numpy as np
+import pandas as pd
+from prettytable import PrettyTable
+
+from binance.client import Client
+from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
+
+
+logging.basicConfig(
+    level=logging.ERROR,
+    filename=os.path.basename(__file__) +
+    '.log',
+    format='{asctime} [{levelname:8}] {process} {thread} {module}: {message}',
+    style='{')
+
+parser = argparse.ArgumentParser(description='Binance Signals')
+parser.add_argument(
+    '--symbol',
+    '-s',
+    nargs='?',
+    const=1,
+    type=str,
+    default='ADAUSDT',
+    help='symbol flag')
+
+args = parser.parse_args()
+
+if args.symbol:
+    markets = {args.symbol.upper()}
+
+    api_key = os.environ.get('binance_api')
+    api_secret = os.environ.get('binance_secret')
+
+    client = Client(api_key, api_secret)
+
+    binance_manager = BinanceWebSocketApiManager(
+        exchange='binance.com', output_default='dict')
+
+    cols = {
+        't': 'start_time',
+        'T': 'end_time',
+        's': 'symbol',
+        'i': 'interval',
+        'f': 'first_trade_id',
+        'L': 'last_trade_id',
+        'o': 'open',
+        'c': 'close',
+        'h': 'high',
+        'l': 'low',
+        'v': 'volume',
+        'n': 'number_of_trades',
+        'x': 'final_bar',
+        'q': 'quote_volume',
+        'V': 'volume_of_active_buy',
+        'Q': 'quote_volume_of_active_buy'
+    }
+
+    binance_manager.create_stream('kline_1h', markets)
+
+    result = PrettyTable()
+    result.field_names = [
+        "datetime",
+        "final_bar",
+        "open_bar1",
+        "lowest_low",
+        "pct_change_lowest_low",
+        "anomaly"]
+
+    lowest_low = 0
+
+    while True:
+        if binance_manager.is_manager_stopping():
+            exit(0)
+        stream_data = binance_manager.pop_stream_data_from_stream_buffer()
+        if stream_data is False:
+            time.sleep(0.01)
+        else:
+            if stream_data is not None:
+                if 'data' in stream_data.keys():
+                    df = pd.DataFrame([stream_data['data']['k']])
+                    df['datetime'] = pd.Timestamp('today')
+
+                    df.rename(columns=cols, inplace=True)
+                    df.set_index('datetime', drop=True, inplace=True)
+                    df = df.iloc[0]
+
+                    open_bar1 = float(
+                        client.get_historical_klines(
+                            args.symbol,
+                            Client.KLINE_INTERVAL_1HOUR,
+                            "2 hour ago UTC")[0][1])
+                    df['pct_change_low'] = (
+                        ((float(df['low']) - open_bar1) / open_bar1) * 100)
+
+                    if df['pct_change_low'] < lowest_low:
+                        lowest_low = df['pct_change_low']
+
+                    df['pct_change_lowest_low'] = lowest_low
+
+                    anomaly = np.where(
+                        df['pct_change_lowest_low'] <= -(20 * 0.9), True, False)
+
+                    result.add_row([df.name, df['final_bar'], open_bar1, lowest_low, round(
+                        df['pct_change_lowest_low'], 4), anomaly])
+                    print(result)
+
+                    if df['final_bar']:
+                        lowest_low = 0
+else:
+    print('Missing symbol')
